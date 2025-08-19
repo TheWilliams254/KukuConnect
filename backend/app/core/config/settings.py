2707878484eb77cee8698aa9cde19a85d02a98af
william <williamwambugu663@gmail.com>
2025-08-19 12:07:08 +0300

    main and .env one step up

--- backend/app/core/config/settings.py
+++ backend/app/core/config/settings.py
@@ -5,12 +5,13 @@ class Settings(BaseSettings):
     DATABASE_URL: str
     SECRET_KEY: str
     ALGORITHM: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
     ADMIN_USERNAME: str
     ADMIN_PASSWORD: str
     ADMIN_EMAIL: str
 
     class Config:
+        extra = "forbid"
         env_file = ".env"
 
 
